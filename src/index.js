0c321f849842c63764a67669e3cc8fae4030c3db
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-08-12 13:00:00 +0530

    Updating State & Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -12,17 +12,33 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props); // Reference to Constructor of Parent Class (React.Component)
-        this.state = { lat: null };
+    
+        // THIS IS THE ONLY TIME we do direct assignment to this.state
+        this.state = { lat: null, errorMessage: '' };
+        
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // We called setState !!
+                this.setState({ lat: position.coords.latitude });
+                // We did not : "this.state.lat = position.coords.latitude"
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
     }
     // React says we have to define render !!
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div> Latitude : </div>;
+        // Conditional Rendering
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>;
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <div> Latitude: {this.state.lat} </div>;
+        }
+        return <div> Loading ! </div>
     }
-}
+};
 
 ReactDOM.render(
     <App />,
